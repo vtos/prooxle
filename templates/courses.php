b7b592337d616689ceddff2d2b95781901abda8e
Vitaly Potenko <potenkov@gmail.com>
2021-10-31 16:34:28 +0300

    Set up basic courses layout.

---
+++ templates/courses.php
@@ -0,0 +1,57 @@
+<?php
+
+$this->layout('main', [
+    'title' => 'Our Courses',
+]);
+?>
+
+<h1>Our Courses</h1>
+<p class="lead">
+    Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.
+</p>
+<p class="lead">
+    <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+</p>
+
+<div class="courses-container row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+    <div class="col">
+        <div class="card text-black">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+            <div class="card-body">
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+
+            <div class="card-footer">
+                <p class="card-text">Some stuff</p>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card text-black">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+            <div class="card-body">
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+
+            <div class="card-footer">
+                <p class="card-text">Some stuff</p>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card text-black">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+            <div class="card-body">
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+
+            <div class="card-footer">
+                <p class="card-text">Some stuff</p>
+            </div>
+        </div>
+    </div>
+</div>
+
